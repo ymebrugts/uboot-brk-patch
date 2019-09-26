71452dd1e69c09e035fe3f906ebb8d2e0db2144d
Fabien Dessenne <fabien.dessenne@st.com>
2019-09-26 16:03:57 +0200

    remoteproc: stm32: load resource table from firmware
    Load the optional resource table from the firmware, and write its
    address in the dedicated backup register.
    
    Change-Id: Ia9622ce1565256b89d9311bb2628d108802fc048
    Signed-off-by: Fabien Dessenne <fabien.dessenne@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/u-boot/+/147987
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Patrick DELAUNAY <patrick.delaunay@st.com>

--- drivers/remoteproc/stm32_copro.c
+++ drivers/remoteproc/stm32_copro.c
@@ -24,6 +24,7 @@
  * @hold_boot_offset:	offset of the register controlling the hold boot setting
  * @hold_boot_mask:	bitmask of the register for the hold boot field
  * @secured_soc:	TZEN flag (register protection)
+ * @rsc_table_addr:	resource table address
  */
 struct stm32_copro_privdata {
 	struct reset_ctl reset_ctl;
@@ -31,6 +32,7 @@ struct stm32_copro_privdata {
 	uint hold_boot_offset;
 	uint hold_boot_mask;
 	bool secured_soc;
+	ulong rsc_table_addr;
 };
 
 /**
@@ -173,6 +175,7 @@ static void *stm32_copro_device_to_virt(struct udevice *dev, ulong da,
 static int stm32_copro_load(struct udevice *dev, ulong addr, ulong size)
 {
 	struct stm32_copro_privdata *priv;
+	ulong rsc_table_size;
 	int ret;
 
 	priv = dev_get_priv(dev);
@@ -187,6 +190,12 @@ static int stm32_copro_load(struct udevice *dev, ulong addr, ulong size)
 		return ret;
 	}
 
+	if (rproc_elf32_load_rsc_table(dev, addr, size, &priv->rsc_table_addr,
+				       &rsc_table_size)) {
+		priv->rsc_table_addr = 0;
+		dev_warn(dev, "No valid resource table for this firmware\n");
+	}
+
 	return rproc_elf32_load_image(dev, addr, size);
 }
 
@@ -197,7 +206,10 @@ static int stm32_copro_load(struct udevice *dev, ulong addr, ulong size)
  */
 static int stm32_copro_start(struct udevice *dev)
 {
+	struct stm32_copro_privdata *priv;
 	int ret;
+
+	priv = dev_get_priv(dev);
 
 	/* move hold boot from true to false start the copro */
 	ret = stm32_copro_set_hold_boot(dev, false);
@@ -211,6 +223,10 @@ static int stm32_copro_start(struct udevice *dev)
 	ret = stm32_copro_set_hold_boot(dev, true);
 	writel(ret ? TAMP_COPRO_STATE_OFF : TAMP_COPRO_STATE_CRUN,
 	       TAMP_COPRO_STATE);
+	if (!ret)
+		/* Store rsc_address in bkp register */
+		writel(priv->rsc_table_addr, TAMP_COPRO_RSC_TBL_ADDRESS);
+
 	return ret;
 }
 
